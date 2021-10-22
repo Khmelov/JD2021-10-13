79a3dcf99645075555ba9c560c9aa97b2ca9dc81
skarmyzov <skarmyzov@gmail.com>
2021-10-22 12:01:39 +0300

    add all lesson 1-5

---
+++ src/by/it/karmyzov/jd01_03/InOut.java
@@ -0,0 +1,31 @@
+package by.it.karmyzov.jd01_03;
+
+public class InOut {
+
+    public static double[] getArray(String line) {
+
+        String[] strArr = line.split(" ");
+        double[] res = new double[strArr.length];
+        for (int i = 0; i < strArr.length; i++) {
+            res[i] = Double.parseDouble(strArr[i]);
+        }
+
+        return res;
+    }
+
+    static void printArray(double[] array) {
+        for (double element : array) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+    }
+
+    public static void printArray(double[] array, String name, int columnCount) {
+        for (int i = 0; i < array.length; i++) {
+            //V[1}=12.1234
+            System.out.printf("%s[% -3d]=%-10.4f ", name, i, array[i]);
+            if ((i + 1) % columnCount == 0 || i + 1 == array.length)
+                System.out.println();
+        }
+    }
+}
