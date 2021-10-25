f9dab57624ee2d6c80ecf10f7cb94b70db8a088b
kirikant <kirikant@yandex.ru>
2021-10-25 14:51:06 +0300

    5+6

--- src/by/it/antipov/jd01_04/TaskC.java
+++ src/by/it/antipov/jd01_04/TaskC.java
@@ -2,17 +2,17 @@ package by.it.antipov.jd01_04;
 
 public class TaskC {
     public static void main(String[] args) {
-double []array={-4,-8,2,3,4,5,6,7,8,9,10,11,13};
-binarySearch(array,13);
+double []array={-8,-4,2,3,4,5,6,7,8,9,10,11,13,14};
+binarySearch(array,-4);
     }
 
     static int binarySearch (double[]array,double value){
         int n= (array.length-1)/2;
         double nD = (double)(array.length-1)/2;
-double dist = Math.abs((n-(array.length-1))/2);
+double dist = (array.length-1)/2;
         for (int i = 0; i <Math.log(array.length)/Math.log(2); i++) {
-if (value>array[n]){if(i==0) {nD=(n+array.length-1)/2;n=(n+array.length-1)/2;} if(i!=0) {nD=((n+(nD+dist))/2);n= (int) ((n+(nD+dist))/2);dist= Math.abs((n-(dist))/2);}}
-if (value<array[n]) {if (i==0) {nD=n/2;n=(n/2);} if(i!=0) {nD=((n+(nD+dist))/2);n=(int)((n+(nD-dist))/2);dist= Math.abs((n-(dist))/2);} }
+if (value>array[n])  {nD=((n+(nD+dist))/2);n= (int)(Math.round((n+(nD+dist))/2));dist= dist/2;}
+if (value<array[n])  {nD=((n+(nD-dist))/2);n=(int)(Math.round((n+(nD-dist))/2));dist= dist/2;}
 if (value==array [n]) {System.out.println(n);break;}
 
         }
