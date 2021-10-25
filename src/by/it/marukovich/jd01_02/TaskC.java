6339af32a632b3dfa9748e5ce90517be998b2ec9
Maksim <maksim.marukovich.00@mail.ru>
2021-10-25 11:42:08 +0300

    some improvements on code

--- src/by/it/marukovich/jd01_02/TaskC.java
+++ src/by/it/marukovich/jd01_02/TaskC.java
@@ -9,20 +9,27 @@ public class TaskC {
         int n = scanner.nextInt();
         int[][] step1;
 
-        System.out.println();
-
     }
     static int[][] step1(int n){
 
         int [][] arr = new int[n][n];
-        boolean min = false;
-        boolean max = false;
+        boolean isNegativeNHere = false;
+        boolean isPositiveNHere = false;
+        do{
 
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
                     arr[i][j] = (int) (Math.random() * (n * 2 + 1)) - n;
+                    if (arr[i][j]==-n){
+                        isNegativeNHere = true;
+                    } else if (arr[i][j]==n){
+                        isPositiveNHere = true;
+                    }
+
                 }
             }
+        }
+        while (!(isNegativeNHere) && isPositiveNHere);
 
 
         return arr;
