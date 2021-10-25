ae0331cab6b8045f6e58c6c357157aae649335ad
Karpovich <kdv503@gmail.com>
2021-10-25 11:44:45 +0300

    25.10.2021 jd01_06 start

--- src/by/it/karpovich/jd01_06/TaskC2.java
+++ src/by/it/karpovich/jd01_06/TaskC2.java
@@ -1,4 +1,9 @@
 package by.it.karpovich.jd01_06;
 
 public class TaskC2 {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer();
+        System.out.println("Длина --> " + sb.length());
+    }
+
 }
