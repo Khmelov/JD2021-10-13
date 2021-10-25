b46ad50bee8e79809d6eb63be2a4f32b5ff6dc7f
Karpovich <kdv503@gmail.com>
2021-10-25 12:26:11 +0300

    25.10.2021 jd01_06 A1_A2 OK

--- src/by/it/karpovich/jd01_06/TaskA2.java
+++ src/by/it/karpovich/jd01_06/TaskA2.java
@@ -1,4 +1,40 @@
 package by.it.karpovich.jd01_06;
 
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TaskA2 {
+    private static String[] w={};
+    private static int[] count={};
+    private static int pos(String word){
+        for (int i = 0; i < w.length; i++) {
+            if(w[i].equals(word)) return i;
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+
+        Pattern pattern = Pattern.compile("[а-яА-ЯёЁ]+");
+        Matcher matcher = pattern.matcher(Poem.text);
+        while (matcher.find()) {
+
+            String word = matcher.group();
+            int p = pos(word);
+            if (p>=0) {
+                count[p]++;
+            }
+            else{
+                int last = w.length; // Последний элемент
+                w= Arrays.copyOf(w,last+1);
+                w[last]=word; // Запомним слово
+                count = Arrays.copyOf(count,last+1);
+                count[last]=1; // Указываем частоту повторений = 1
+            }
+        }
+        for (int i = 0; i < w.length; i++) {
+            System.out.println(w[i]+"="+count[i]);
+        }
+
+    }
 }
