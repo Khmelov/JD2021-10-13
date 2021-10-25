c8b260dcb0c87f1bcab0271673a847e3b37b5019
Dmitry <ode2001@mail.ru>
2021-10-25 12:34:04 +0300

    ok

--- src/by/it/oliakhevich/jd01_05/TaskB.java
+++ src/by/it/oliakhevich/jd01_05/TaskB.java
@@ -12,11 +12,15 @@ public class TaskB {
     }
 
     private static void step3() {
-
+        double calc = 0;
         for (double a = 0; a <= 2; a = a + 0.2) {
-            // for (int x = 1; x <= 6; x++) {
-            double y = (pow(7, a)); //- cos(a));
-            System.out.printf("При а=%-4s y=%-9s\n", a, y);
+            System.out.println(calc);
+            for (int x = 1; x <=6 ; x++) {
+                calc = calc+(pow(7, a))- cos(x);
+
+            }
+
+
         }
 
 
@@ -29,16 +33,16 @@ public class TaskB {
             double a = log10(abs(sin(x) + 2.74));
 
             if (x / 2 > -2 && x / 2 <= -1) {
-                a = log10(abs(sin(pow((x / 2), 2)) + 2.74));
-                System.out.printf("При x/2 =%-1.2f a=%e\n", x / 2, a);
+                a = log10(abs(sin(pow(x, 2)) + 2.74));
+                System.out.printf("При x/2 =%x / 2-1.2f a=%e\n", x / 2, a);
             }
 
             if (x / 2 > -1 && x / 2 < 0.2) {
-                a = log10(abs(cos(pow((x / 2), 2)) + 2.74));
+                a = log10(abs(cos(pow(x, 2)) + 2.74));
                 System.out.printf("При x/2=%-1.2f a=%e\n", x / 2, a);
             }
             if (x / 2 == 0.2) {
-                a = log10(abs(tan(pow((x / 2), 2)) + 2.74));
+                a = log10(abs(tan(pow(x , 2)) + 2.74));
                 System.out.printf("При x/2=%-1.2f a=%e\n", x / 2, a);
             }
             if ( x/2 < -2 && x/2 >0.2 ) {
---
+++ src/by/it/oliakhevich/jd01_06/TaskA1.java
@@ -0,0 +1,12 @@
+package by.it.oliakhevich.jd01_06;
+
+import java.util.regex.Pattern;
+
+public class TaskA1 {
+    public static void main(String[] args) {
+
+
+        StringBuilder text = new StringBuilder(Poem.text);
+        // Pattern.matches("[а-яА]")
+    }
+}
