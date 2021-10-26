1cb386af283dda59d611222deacaf647cfd96d92
kirikant <kirikant@yandex.ru>
2021-10-26 23:42:37 +0300

    6

---
+++ src/by/it/antipov/jd01_06/TaskC1.java
@@ -0,0 +1,25 @@
+package by.it.antipov.jd01_06;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TaskC1 {
+    public static void main(String[] args) {
+int max=0;
+        Pattern pattern1 = Pattern.compile("[^\n]+");
+        Pattern pattern2 = Pattern.compile("[ ]+");
+        Matcher match1 = pattern1.matcher(Poem.text);
+        String []array = Poem.text.split("[\n]");
+
+        while (match1.find()) {if (match1.group().length()>max)
+        {max = match1.group().length();}
+        }
+        for (int i = 0; i < array.length; i++) {
+            Matcher match2 =pattern2.matcher(Poem.text);
+            while (array[i].length()!=max){while (match2.find()){if (array[i].length()<max){array[i]=(new StringBuilder(array[i]).insert(match2.end(),' ')).toString();}}
+            }
+System.out.println(array[i]);
+        }
+
+    }
+}
