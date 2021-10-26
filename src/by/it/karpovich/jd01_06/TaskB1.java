c4a7121b16b1a079bdc49ea7e76cca7802fbdfa6
Karpovich <kdv503@gmail.com>
2021-10-26 11:21:36 +0300

    26.10.2021 Start B1

--- src/by/it/karpovich/jd01_06/TaskB1.java
+++ src/by/it/karpovich/jd01_06/TaskB1.java
@@ -6,14 +6,38 @@ import java.util.regex.Pattern;
 public class TaskB1 {
  //   Вывести в консоль все слова текста, начинающихся согласной и заканчивающихся
  //  гласной буквой (нужно сделать для проверки этого условия приватный метод без regex).
+ // АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя
+    // аАоОэЭиИуУыЫеЕёЁюЮяЯ
+    // аоэиуыеёюя
 
     public static void main(String[] args) {
-
+        step1();
     }
 
-
-
-
+   /* private static void step1(){
+        StringBuilder sb = new StringBuilder(Poem.text);
+        Pattern pattern = Pattern.compile("[АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя]{1}");
+        Matcher matcher = pattern.matcher(Poem.text);
+        while (matcher.find()) {
+            int start = matcher.start();
+            sb.setCharAt(start, '#');
+            // if (matcher.group().length() >= 7) {
+            //    sb.setCharAt(start, '#');
+            //  }
+        }
+        System.out.println(sb);
+    }*/
+   private static void step1() {
+       String text = Poem.text;
+       String[] wordArray = text.split("[\\s,.:!?]+");
+       Pattern pattern = Pattern.compile("[^ИаАоОэЭуУыЫеЕёЁюЮяЯ].*[аАоОэЭиИуУыЫеЕёЁюЮяЯ]$");
+       for (String word : wordArray) {
+           Matcher matcher = pattern.matcher(word);
+           if (matcher.find()) {
+               System.out.println(word);
+           }
+       }
+   }
 
 
 
