bd22316e4ea0bbfccd085b0d2324171773963550
Maksim <maksim.marukovich.00@mail.ru>
2021-10-27 09:41:38 +0300

    some code improvements

--- src/by/it/marukovich/jd01_01/TaskB3.java
+++ src/by/it/marukovich/jd01_01/TaskB3.java
@@ -11,7 +11,14 @@ package by.it.marukovich.jd01_01;
 */
 class TaskB3 {
     public static void main(String[] args) {
+//        String s = "C";
+//        System.out.println("началом курса");
+//        System.out.println("C началом");
+//        System.out.println("курса");
         System.out.print("C началом");
+//        System.out.println("курса");
+//        System.out.println(s);
+//        System.out.print("C началом!");
         System.out.print(" ");
         System.out.println("курса");
     }
