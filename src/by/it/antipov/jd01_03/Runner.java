117ad110240ab88300eca43c5909dada333bfbfc
kirikant <kirikant@yandex.ru>
2021-10-28 00:11:45 +0300

    C01_03+C01_04

--- src/by/it/antipov/jd01_03/Runner.java
+++ src/by/it/antipov/jd01_03/Runner.java
@@ -3,5 +3,23 @@ package by.it.antipov.jd01_03;
 import java.util.Scanner;
 
 public class Runner {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String value = "value";
+        double[] vector = {1, 2, 3};
+        double[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        double[][] left = {{1, 2, 3},{4, 5, 6}};
+
+        double[][] right = {
+                {1, 2},
+                {3, 4},
+                {5, 6}
+        };
+       Helper.multiply(left,vector);
+
+    }
 }
