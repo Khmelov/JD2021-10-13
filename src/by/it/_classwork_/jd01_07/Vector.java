f30f6264ef084a462ec68af05ba8e19223d5e493
jhon Doe <scokpius@gmail.com>
2021-10-27 12:26:12 +0300

    lesson 7 started

---
+++ src/by/it/_classwork_/jd01_07/Vector.java
@@ -0,0 +1,30 @@
+package by.it._classwork_.jd01_07;
+
+import java.util.Arrays;
+
+public class Vector extends Var {
+
+    private final double[] values;
+
+    public Vector(double[] values) {
+        this.values = Arrays.copyOf(values, values.length);
+    }
+
+    public double[] getValues() {
+        return Arrays.copyOf(values, values.length);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder out = new StringBuilder();
+        out.append("{");
+        String delimiter = "";
+        for (double value : values) {
+            out.append(delimiter);
+            out.append(value);
+            delimiter = ", ";
+        }
+        out.append("}");
+        return out.toString();
+    }
+}
