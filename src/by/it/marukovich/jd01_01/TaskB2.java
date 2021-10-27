7f5aa96d7bd4be709045e2f21331e0e158a35a5c
Maksim <maksim.marukovich.00@mail.ru>
2021-10-27 09:44:11 +0300

    some code improvements

--- src/by/it/marukovich/jd01_01/TaskB2.java
+++ src/by/it/marukovich/jd01_01/TaskB2.java
@@ -16,10 +16,10 @@ package by.it.marukovich.jd01_01;
 
  */
 class TaskB2 {
-    private static int a = 1;
-    private static int b = 3;
-    private static int c = 9;
-    private static int d = 27;
+    private static final int a = 1;
+    private static final int b = 3;
+    private static final int c = 9;
+    private static final int d = 27;
 
     public static void main(String[] args) {
 
