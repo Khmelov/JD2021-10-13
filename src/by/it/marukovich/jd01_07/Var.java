a2082191ccf6e6b776c843895723e6eb9eaeab2d
Maksim <maksim.marukovich.00@mail.ru>
2021-10-27 12:26:26 +0300

    Lesson 7 Task A and B1

--- src/by/it/marukovich/jd01_07/Var.java
+++ src/by/it/marukovich/jd01_07/Var.java
@@ -2,7 +2,7 @@ package by.it.marukovich.jd01_07;
 
 public abstract class Var {
     @Override
-    public String toString() {
+    public String toString(){
         return "Unknown variable (abstract stub)";
     }
 }
