8bd85d7d256478ee24a39c6bd7c655f239a29e1b
kirikant <kirikant@yandex.ru>
2021-10-29 00:30:43 +0300

    01_06 B

--- src/by/it/antipov/jd01_06/TaskB2.java
+++ src/by/it/antipov/jd01_06/TaskB2.java
@@ -6,35 +6,42 @@ import java.util.regex.Pattern;
 public class TaskB2 {
     public static void main(String[] args) {
         StringBuilder text1 = new StringBuilder(Poem.text);
-        Pattern pattern1 = Pattern.compile("[^.!]+");
-        Pattern pattern2 = Pattern.compile("[^а-яА-ЯёЁ]+");
+        Pattern pattern1 = Pattern.compile("[.!]+");
+      //  Pattern pattern2 = Pattern.compile("[^а-яА-ЯёЁ]+");
         Pattern pattern3 = Pattern.compile("[^.!]+");
         Matcher match1 = pattern1.matcher(text1);
         Matcher match3 = pattern3.matcher(text1);
        int i=0;
         while (match1.find()) {i++;}
-        System.out.println(i);
+     //   System.out.println(i);
       String []sentences = new String[i];
-        int f=0;
-        while (match3.find()) { // два одинаковых цикла с мэтч не работают?
+       // int f=0;
+
+        for (int j = 0; j <i; j++) {
+         match3.find();
             String sentence = match3.group();
-            System.out.println(sentence);
-            Matcher match2 = pattern2.matcher(sentence);
-           String sentence1 = sentence.replace(match2.group()," ");
-           String sentence2 = sentence1.trim();
-            sentences[f]=sentence2;
-            System.out.println(sentences[f]);
-            f++;
+         //   Matcher match2 = pattern2.matcher(sentence);
+            String sentence1 = sentence;
+           // while (match2.find()) {}
+                sentence1 = sentence1.replaceAll("[^а-яА-ЯёЁ]+", " ");
+            String sentence2 = sentence1.trim();
+            sentences[j] = sentence2;
+            System.out.println(sentences[j]);
         }
-        for (int j = 0; j < sentences.length; j++) {
+        for (int m = 0; m < sentences.length-1; m++) {for (int j = 0; j < sentences.length-1; j++) {
             if (sentences[j+1].length()<sentences[j].length()) {String buffer = sentences[j+1];
-            sentences[j+1]=sentences[j];
-            sentences[j]=buffer;
-            }
+                sentences[j+1]=sentences[j];
+                sentences[j]=buffer;
+            }}
+
+        }
+
+
             for (int k = 0; k < sentences.length; k++) {
                 System.out.println(sentences[k]);
 
             }
         }
     }
-}
+
+
