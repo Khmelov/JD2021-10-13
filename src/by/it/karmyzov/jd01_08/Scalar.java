9e734076e85d64baf516200f7d7060c4c7e042bc
skarmyzov <skarmyzov@gmail.com>
2021-10-30 16:32:49 +0300

    Lesson 6 started

--- src/by/it/karmyzov/jd01_08/Scalar.java
+++ src/by/it/karmyzov/jd01_08/Scalar.java
@@ -18,6 +18,52 @@ public class Scalar extends Var {
         return Double.toString(value);
     }
 
+    @Override
+    public Var add(Var other) {
+        if (other instanceof Scalar) {
+            double sum = this.value + ((Scalar) other).value;
+            return new Scalar(sum);
+        }
+else
+    return other.add(this);
+    }
+
+    @Override
+    public Var sub(Var other) {
+          if (other instanceof Scalar) {
+            double sub = this.value - ((Scalar) other).value;
+            return new Scalar(sub);
+        }
+        else
+            return new Scalar(-1).mul(other).add(this);
+    }
+
+    @Override
+    public Var mul(Var other) {
+        if (other instanceof Scalar) {
+            double mul = this.value * ((Scalar) other).value;
+            return new Scalar(mul);
+        }
+        else
+            return other.mul(this);
+    }
+
+    @Override
+    public Var div(Var other) {
+        if (other instanceof Scalar) {
+            double div = this.value / ((Scalar) other).value;
+            return new Scalar(div);
+        }
+        else
+            return super.div(other);
+    }
 
     }
+
+
+
+
+
+
+
 
