2572180011f0f1f99d46eea75ec67a100484099b
ddshcharbunou <danielshcharbunou@gmail.com>
2021-11-02 12:59:11 +0300

    start jd01_08

--- src/by/it/shcharbunou/jd01_08/Runner.java
+++ src/by/it/shcharbunou/jd01_08/Runner.java
@@ -1,4 +1,8 @@
 package by.it.shcharbunou.jd01_08;
 
 public class Runner {
+
+    public static void main(String[] args) {
+
+    }
 }
---
+++ src/by/it/shcharbunou/jd01_08/Var.java
@@ -0,0 +1,9 @@
+package by.it.shcharbunou.jd01_08;
+
+public abstract class Var {
+
+    @Override
+    public String toString() {
+        return "unknown variable (abstract stub)";
+    }
+}
