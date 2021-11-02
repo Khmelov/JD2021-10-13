9a1acb9476885ae681545d1c968063baf850def2
skarmyzov <skarmyzov@gmail.com>
2021-11-02 14:51:31 +0300

    Lesson 6 started

---
+++ src/by/it/karmyzov/jd01_0901/Var.java
@@ -0,0 +1,37 @@
+package by.it.karmyzov.jd01_0901;
+
+
+import by.it.karmyzov.jd01_09.Patterns;
+
+abstract class Var implements Operation {
+
+    @Override
+    public Var add(Var other) {
+        System.out.println("Операция сложения " + this + "+" + other + " " + "невозможна");
+        return null;
+    }
+
+    @Override
+    public Var sub(Var other) {
+        System.out.println("Операция вычитания " + this + "-" + other + " " + "невозможна");
+        return null;
+    }
+
+    @Override
+    public Var mul(Var other) {
+        System.out.println("Операция умножения " + this + "*" + other + " " + "невозможна");
+        return null;
+    }
+
+    @Override
+    public Var div(Var other) {
+        System.out.println("Операция деления  " + this + "/" + other + " " + "невозможна");
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return ("unknown variable (abstract stub)");
+
+    }
+}
