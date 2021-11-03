e1e496115272e3640721121bec5184a922230e03
AsyaMal <asyamalyschik@gmail.com>
2021-11-03 22:00:44 +0300

    add HW 9 Tasks B

--- src/by/it/malyshchik/jd01_09/Parser.java
+++ src/by/it/malyshchik/jd01_09/Parser.java
@@ -1,26 +1,46 @@
 package by.it.malyshchik.jd01_09;
 
+
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Parser {
-    Var calc(String expression){
-        String[] operand = expression.split(Patterns.OPERATION);
-        Var one = Var.createVar(operand[0]);
-        Var two = Var.createVar(operand[1]);
-        if (one==null || two==null)
-            return null;
-        Pattern p = Pattern.compile(Patterns.OPERATION);
-        Matcher m = p.matcher(expression);
-        if (m.find()){
-            String operation = m.group();
-            switch (operation){
-                case "+": return one.add(two);
-                case "-": return one.sub(two);
-                case "*": return one.mul(two);
-                case "/": return one.div(two);
+    Var calc(String expression) {
+        expression = expression.trim().replace(" ", "");
+        String[] parts = expression.split(Patterns.OPERATION, 2);
+        String leftPart = parts[0];
+        Var left = VarCreator.createVar(leftPart);
+        if (Objects.isNull(left)) {
+            System.out.println("Incorrect variable " + leftPart);
+            return left;
+        }
+        if (parts.length == 1) {
+            return left;
+        }
+        String rightPart = parts[1];
+        Var right = VarCreator.createVar(rightPart);
+        if (Objects.isNull(right)) {
+            System.out.println("Incorrect variable " + rightPart);
+            return right;
+        }
+
+        Pattern patternOperation = Pattern.compile(Patterns.OPERATION);
+        Matcher matcher = patternOperation.matcher(expression);
+        if (matcher.find()) {
+            String operation = matcher.group();
+            switch (operation) {
+                case "+":
+                    return left.add(right);
+                case "-":
+                    return left.sub(right);
+                case "*":
+                    return left.mul(right);
+                case "/":
+                    return left.div(right);
             }
         }
+        System.out.println("Something stupid");
         return null;
     }
 }
