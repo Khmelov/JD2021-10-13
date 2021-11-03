c91575ce71d7351f904fdfc33d578cd993c476e8
ddshcharbunou <danielshcharbunou@gmail.com>
2021-11-03 20:20:25 +0300

    jd01_09 fix antipattern const interface

--- src/by/it/shcharbunou/jd01_09/Samples.java
+++ src/by/it/shcharbunou/jd01_09/Samples.java
@@ -1,8 +1,8 @@
 package by.it.shcharbunou.jd01_09;
 
-public interface Samples {
-    String OPERATION = "[+-*/]";
-    String SCALAR = "-?[0-9]+(\\.[0-9]+)?";
-    String VECTOR = "\\{" + SCALAR + "(," + SCALAR + ")*}";
-    String MATRIX = "\\{" + VECTOR + "(," + VECTOR + ")*}";
+public final class Samples {
+    public static final String OPERATION = "[+-*/]";
+    public static final String SCALAR = "-?[0-9]+(\\.[0-9]+)?";
+    public static final String VECTOR = "\\{" + SCALAR + "(," + SCALAR + ")*}";
+    public static final String MATRIX = "\\{" + VECTOR + "(," + VECTOR + ")*}";
 }
--- src/by/it/shcharbunou/jd01_09/VarCreator.java
+++ src/by/it/shcharbunou/jd01_09/VarCreator.java
@@ -1,13 +1,15 @@
 package by.it.shcharbunou.jd01_09;
 
+import static by.it.shcharbunou.jd01_09.Samples.*;
+
 public class VarCreator {
 
     protected static Var createVar(String stringOperand) {
-        if (stringOperand.matches(Samples.SCALAR)) {
+        if (stringOperand.matches(SCALAR)) {
             return new Scalar(stringOperand);
-        } else if (stringOperand.matches(Samples.VECTOR)) {
+        } else if (stringOperand.matches(VECTOR)) {
             return new Vector(stringOperand);
-        } else if (stringOperand.matches(Samples.MATRIX)) {
+        } else if (stringOperand.matches(MATRIX)) {
             return new Matrix(stringOperand);
         } else {
             return null;
