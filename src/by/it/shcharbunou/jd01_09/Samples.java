6732cfa5933f2a874f6afbc7d68abd9e77254151
ddshcharbunou <danielshcharbunou@gmail.com>
2021-11-03 20:10:56 +0300

    jd01_09 realized Samples and VarCreator start Parser fix some warnings

--- src/by/it/shcharbunou/jd01_09/Samples.java
+++ src/by/it/shcharbunou/jd01_09/Samples.java
@@ -1,8 +1,8 @@
 package by.it.shcharbunou.jd01_09;
 
 public interface Samples {
-    static final String OPERATION = "[+-*/]";
-    static final String SCALAR = "-?[0-9]+(\\.[0-9]+)?";
-    static final String VECTOR = "\\{" + SCALAR + "(," + SCALAR + ")*}";
-    static final String MATRIX = "\\{" + VECTOR + "(," + VECTOR + ")*}";
+    String OPERATION = "[+-*/]";
+    String SCALAR = "-?[0-9]+(\\.[0-9]+)?";
+    String VECTOR = "\\{" + SCALAR + "(," + SCALAR + ")*}";
+    String MATRIX = "\\{" + VECTOR + "(," + VECTOR + ")*}";
 }
