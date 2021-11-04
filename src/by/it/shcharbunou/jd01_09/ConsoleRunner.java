0fd5e484d6a3b99c3ef586f6e6f653cce0372f7f
ddshcharbunou <danielshcharbunou@gmail.com>
2021-11-04 12:40:44 +0300

    jd01_09 add InputManager add ApplicationsReservedWords start ConsoleRunner

--- src/by/it/shcharbunou/jd01_09/ConsoleRunner.java
+++ src/by/it/shcharbunou/jd01_09/ConsoleRunner.java
@@ -3,6 +3,11 @@ package by.it.shcharbunou.jd01_09;
 public class ConsoleRunner {
 
     public static void main(String[] args) {
+        runApplication();
+    }
+
+    private static void runApplication() {
+        InputManager inputManager = new InputManager();
 
     }
 }
