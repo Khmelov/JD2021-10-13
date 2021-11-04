3cbdedc1ddba63804c82b156d6fd5fc93b15ea96
YekaterinaRudakova <radkevichch@gmail.com>
2021-11-04 15:05:04 +0300

    lesson 10,Task C

---
+++ src/by/it/rudakova/jd01_10/BeanTester.java
@@ -0,0 +1,23 @@
+package by.it.rudakova.jd01_10;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+public class BeanTester {
+    public static void main(String[] args) throws Exception {
+        Class<?> c = Bean.class;
+        Constructor<?> constructor=c.getConstructor();
+        Object finder=constructor.newInstance();
+
+        Method[] methods=c.getDeclaredMethods();
+
+        for(Method method:methods){
+            if(method.isAnnotationPresent(Param.class)){
+                Param foundAnnotation=method.getAnnotation(Param.class);
+                Object result= method.invoke(finder, foundAnnotation.a(),foundAnnotation.b());
+                System.out.println(method.getName()+" "+result);
+            }
+        }
+    }
+}
