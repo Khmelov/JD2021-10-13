fe13e06e4e86122275af8ffe1ca85374e57fcb8f
ddshcharbunou <danielshcharbunou@gmail.com>
2021-11-07 01:11:13 +0300

    add full jd01_11

--- src/by/it/shcharbunou/jd01_11/Runner.java
+++ src/by/it/shcharbunou/jd01_11/Runner.java
@@ -1,37 +1,12 @@
 package by.it.shcharbunou.jd01_11;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Runner {
 
     public static void main(String[] args) {
-        ListA<Integer> list = new ListA<>();
-        list.add(3);
-        list.add(4);
-        list.add(5);
-        list.add(6);
-        list.add(7);
-        list.add(8);
-        list.add(9);
-        list.add(3123);
-        list.add(345);
-        list.add(754);
-        list.add(76);
-        list.add(56);
-        list.add(34);
-        list.add(9);
-        System.out.println(list.size());
-        System.out.println(list);
-        list.remove(4);
-        System.out.println(list);
-        System.out.println(list.get(0));
-        ListB<Long> listB = new ListB<>();
-        ArrayList<Integer> arrayListB = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            listB.add(i * 2L);
-        }
-        System.out.println(listB);
-        listB.add(9, null);
-        System.out.println(listB);
+        SetC<Integer> set = new SetC<>();
+        set.add(23);
+        System.out.println(set);
     }
 }
