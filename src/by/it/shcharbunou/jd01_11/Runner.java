c6ba625ba75222645d0fad3bea69962d923221e2
ddshcharbunou <danielshcharbunou@gmail.com>
2021-11-07 01:12:23 +0300

    add full jd01_11

--- src/by/it/shcharbunou/jd01_11/Runner.java
+++ src/by/it/shcharbunou/jd01_11/Runner.java
@@ -1,12 +1,8 @@
 package by.it.shcharbunou.jd01_11;
 
-import java.util.HashSet;
-
 public class Runner {
 
     public static void main(String[] args) {
-        SetC<Integer> set = new SetC<>();
-        set.add(23);
-        System.out.println(set);
+        // checked while building...
     }
 }
