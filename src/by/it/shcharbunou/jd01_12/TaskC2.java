9f6277bae3683f3582488711b8be09b4b580d882
ddshcharbunou <danielshcharbunou@gmail.com>
2021-11-12 10:19:29 +0300

    FIX FILES

--- src/by/it/shcharbunou/jd01_12/TaskC2.java
+++ src/by/it/shcharbunou/jd01_12/TaskC2.java
@@ -17,7 +17,6 @@ public class TaskC2 {
         return crossSet;
     }
 
-
     private static Set<?> getUnion(Set<?> ... setsArray) {
         Set<?> unionSet = new HashSet<>(setsArray[0]);
         for (int i = 0; i < setsArray.length; i++) {
