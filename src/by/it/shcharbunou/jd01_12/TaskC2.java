011c45fb1b7223867077f2f4b74d79c390e71c47
ddshcharbunou <danielshcharbunou@gmail.com>
2021-11-12 11:33:11 +0300

    jd01_12 add TaskC2 test run

--- src/by/it/shcharbunou/jd01_12/TaskC2.java
+++ src/by/it/shcharbunou/jd01_12/TaskC2.java
@@ -6,18 +6,53 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
+@SuppressWarnings("all")
 public class TaskC2 {
 
     public static void main(String[] args) {
         Set<Integer> b = new HashSet<Integer>(Arrays.asList(4, 3, 5, 6, 7, 8));
         Set<Double> c = new HashSet<Double>(Arrays.asList(0., 1., 2., 3., 4., 5.));
         Set<Number> e = getUnion(b, c);
+        Set<Number> t = getCross(b, c);
         System.out.println(e);
+        System.out.println(t);
     }
 
     @SuppressWarnings("unchecked")
-    private static <T extends Number> Set<T> getCross(Set<? extends T> ... setsArray) {
-        Set<T> crossSet = new HashSet<>(setsArray[0]);
+    private static <T> Set<T> getCross(Set<? extends T>... setsArray) {
+        Set<T> crossSet = new HashSet<T>(setsArray[0]) {
+
+            @Override
+            public boolean retainAll(Collection<?> c) {
+                T[] collectionArray = (T[]) c.toArray();
+                boolean isContains;
+                for (T collectionElement : collectionArray) {
+                    isContains = false;
+                    for (T thisCollectionElement : this) {
+                        if (compareTo(collectionElement, thisCollectionElement) == 0) {
+                            isContains = true;
+                            break;
+                        }
+                    }
+                    if (!isContains) {
+                        c.remove(collectionElement);
+                    }
+                }
+                this.clear();
+                for (Object collectionElement : c) {
+                    this.add((T) collectionElement);
+                }
+                return true;
+            }
+
+            public int compareTo(Object firstObject, Object secondObject) {
+                Number firstNumber = (Number) firstObject;
+                Number secondNumber = (Number) secondObject;
+                BigDecimal firstDecimal = BigDecimal.valueOf(firstNumber.doubleValue());
+                BigDecimal secondDecimal = BigDecimal.valueOf(secondNumber.doubleValue());
+                return firstDecimal.compareTo(secondDecimal);
+            }
+        };
         for (int i = 1; i < setsArray.length; i++) {
             crossSet.retainAll(setsArray[i]);
         }
@@ -25,7 +60,7 @@ public class TaskC2 {
     }
 
     @SuppressWarnings("unchecked")
-    private static <T> Set<T> getUnion(Set<? extends T> ... setsArray) {
+    private static <T> Set<T> getUnion(Set<? extends T>... setsArray) {
         Set<T> unionSet = new HashSet<T>(setsArray[0]) {
 
             @Override
