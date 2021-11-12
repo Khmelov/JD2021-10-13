65fd7ac5912b6aacff9194a47687b411be4e0a47
Karpovich <kdv503@gmail.com>
2021-11-12 04:06:31 +0300

    11.11.2021 = ____________________________________________________________ jd01_01 - OK                                       _________ jd01_02 - ОК                                       _________ jd01_03 - OK                                       _________ jd01_04 - OK                                       _________ jd01_05 - [сделать С2, переделать С1]              _________ jd01_06 - OK                                       _________ jd01_07 - OK                                       _________ jd01_08 - [Доделать С lvl]                         _________ jd01_09 - OK                                       _________ jd01_10 - OK                                       _________ jd01_11 - [В процессе]                             _________ jd01_12 - [START]                                  _________ jd01_13 - [START]                                  _________ jd01_14 - [START]                                  _________ ____________________________________________________________ Test - Введение в ООП на примере создания Person и Student _ my_calculator - Создание калькулятора начало.              _ ____________________________________________________________

---
+++ src/by/it/karpovich/jd01_14/TaskA.java
@@ -0,0 +1,70 @@
+package by.it.karpovich.jd01_14;
+
+import java.io.*;
+
+public class TaskA {
+
+    // Создаем функцию, которая будет определять текущее расположение в которой находится наш Класс:
+    private static String directory(Class<?> cl) {
+        String getDirection = System.getProperty("user.dir") + File.separator + "src" + File.separator;
+        String classDirection = cl.getName().replace(cl.getSimpleName(), "").replace(".", File.separator);
+        return getDirection + classDirection;
+        //user.dir - указывает на наш "каталог проекта"
+        /*          Чтобы не писать разделитель \\ - Windows, // - Линукс, мы используем готовую штуку separator.    */
+
+    }
+
+
+    public static void main(String[] args) {
+        //System.out.println(directory(TaskA.class));
+
+        // Выбираем каталог в который будем записывать файлы:
+        DataOutputStream dos = null;
+        try {
+            dos = new DataOutputStream(
+                    new BufferedOutputStream(
+                            new FileOutputStream(directory(TaskA.class)+"dataTaskA.bin")));
+            // Файловый поток создан, теперь сюда можно записывать данные.
+            for (int i = 0; i < 20; i++) {
+                dos.writeInt((int) Math.random() * 25);
+            }
+        } catch (IOException e) { // Поведение одинаковое, поэтому мы сомкнули всё в один блок.
+            e.printStackTrace();
+        }
+        // Теперь закрываем файл:
+        finally {
+
+            if (dos != null) {
+                try {
+                    dos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+            try (DataInputStream inputStream = new DataInputStream(
+                    new BufferedInputStream(
+                            new FileInputStream(directory(TaskA.class)+"dataTaskA.bin")));
+            PrintWriter out2 = new PrintWriter(new FileWriter(directory(TaskA.class)+"resultTaskA.txt")))
+
+            {
+
+                // Делаем чтение:
+                double sum = 0;
+                double count = 0; // Количество прочитанных элементов.
+// Читаем всех по очереди:
+                while (inputStream.available() >0 ) { //Пока есть что-то в файле
+                    int i = inputStream.readInt(); // То мы читаем каждое число
+                    System.out.print(i + " "); // и печатаем его на экран
+                    out2.print(i + " ");
+                    sum = sum + i;
+                    count++;
+                }
+                System.out.println("\n avg = " +sum/count);
+                out2.print("\n avg = " +sum/count);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+    }
+}
