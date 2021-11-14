d0214a264c9e86d5bd7ed9c3c60a1b08f9dcb564
ddshcharbunou <danielshcharbunou@gmail.com>
2021-11-14 15:08:41 +0300

    jd01_13 add TaskA

---
+++ src/by/it/shcharbunou/jd01_13/TaskA.java
@@ -0,0 +1,43 @@
+package by.it.shcharbunou.jd01_13;
+
+import java.util.HashMap;
+
+@SuppressWarnings("all")
+public class TaskA {
+
+    public static void main(String[] args) {
+        try {
+            if (Math.random() > 0.5)
+                new HashMap<String, String>(null);
+            else
+                Integer.parseInt("привет");
+        } catch (NumberFormatException | NullPointerException e) {
+            printRuntimeException(e);
+        }
+    }
+
+    static void printRuntimeException(RuntimeException e) {
+        Class<by.it.shcharbunou.jd01_13.TaskA> taskAClass = by.it.shcharbunou.jd01_13.TaskA.class;
+        StackTraceElement[] stackTrace = e.getStackTrace();
+        for (StackTraceElement stackTraceElement : stackTrace) {
+            String currentClassName = stackTraceElement.getClassName();
+            if (currentClassName.equals(taskAClass.getName())) {
+                Class<? extends RuntimeException> exceptionClass = e.getClass();
+
+                String classNameException = exceptionClass.getName();
+
+                int lineNumber = stackTraceElement.getLineNumber();
+
+                System.out.printf("" +
+                                " name: %s%n" +
+                                "class: %s%n" +
+                                " line: %d%n",
+                        classNameException,
+                        currentClassName,
+                        lineNumber
+                );
+                break;
+            }
+        }
+    }
+}
