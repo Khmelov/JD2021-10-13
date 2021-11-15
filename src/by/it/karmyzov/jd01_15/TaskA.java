f806eefe5ad1372a90e5f377db5164eaac8c31e9
skarmyzov <skarmyzov@gmail.com>
2021-11-15 15:25:03 +0300

    Lesson 6 started

--- src/by/it/karmyzov/jd01_15/TaskA.java
+++ src/by/it/karmyzov/jd01_15/TaskA.java
@@ -1,29 +1,38 @@
 package by.it.karmyzov.jd01_15;
 
+import java.util.Random;
 
+public class TaskA {
+    public static final int MAX = 15;
+    public static final int MIN = -15;
 
-//import by.it.karmyzov.calc.Patterns;
-//
-//import java.nio.file.Path;
-//import java.util.Random;
-//
-//public class TaskA {
-//    public static void main(String[] args) {
-//        PathFinder serPathfinder = new PathFinder();
-//        int[][] array = new int[6][4];
-//        fillArray(array, min, max)
-//        String filename = PathFinder.getStringPath(TaskA.class, "matrix.txt");
-//
-//    }
-//
-//    private static void fillArray(int[][] array, int MIN, int MAX) {
-//        Random random = new Random();
-//        for (int[] row : array) {
-//            for (int i = 0; i <row.length ; i++) {
-//                row[i] = min +random.nextInt(max-min+1);
-//
-//            }
-//
-//        }
-//    }
-//}
+    public static void main(String[] args) {
+        PathFinder pathFinderService = new PathFinderImpl();
+        int[][] array = new int[6][4];
+        fillArray(array, MIN, MAX);
+        String filename = pathFinderService.getStringPath(TaskA.class, "matrix.txt");
+        RepoMatrix repoMatrix = new RepoMatrixImpl(filename);
+        repoMatrix.save(array);
+        int[][] arrayAfterLoad = repoMatrix.load();
+        printArray(arrayAfterLoad);
+    }
+    private static void printArray (int[][] array) {
+        for (int[] row : array) {
+            for (int element : row) {
+                System.out.printf("%3d ", element);
+            }
+            System.out.println();
+        }
+    }
+
+    private static void fillArray(int[][] array, int min, int max) {
+        Random random = new Random();
+        for (int[] row : array) {
+            for (int i = 0; i < row.length; i++) {
+                row[i] = min + random.nextInt(max - min + 1);
+
+            }
+
+        }
+    }
+}
