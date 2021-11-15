d53582e77441cf03e200e3b5d8a8a66684437f6a
ddshcharbunou <danielshcharbunou@gmail.com>
2021-11-15 08:20:31 +0300

    jd01_13 add TaskC

--- src/by/it/shcharbunou/jd01_13/TaskC.java
+++ src/by/it/shcharbunou/jd01_13/TaskC.java
@@ -1,8 +1,34 @@
 package by.it.shcharbunou.jd01_13;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+@SuppressWarnings("all")
 public class TaskC {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NumberFormatException, InterruptedException {
+        readData();
+    }
 
+    private static void readData() throws NumberFormatException, InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+        List<Double> valuesList = new ArrayList<>();
+        int exceptionsCounter = 0;
+        for (; ; ) {
+            try {
+                valuesList.add(Double.parseDouble(scanner.next()));
+            } catch (NumberFormatException e) {
+                if (exceptionsCounter == 5) {
+                    break;
+                }
+                Thread.sleep(100);
+                for (int i = valuesList.size() - 1; i > -1; i--) {
+                    System.out.print(valuesList.get(i) + " ");
+                }
+                System.out.println();
+                exceptionsCounter++;
+            }
+        }
     }
 }
