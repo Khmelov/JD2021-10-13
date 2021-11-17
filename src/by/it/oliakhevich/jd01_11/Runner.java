82ab22ac2fcbbb3be97c4ebe34e57529cebec5e2
Dmitry <ode2001@mail.ru>
2021-11-17 11:39:04 +0300

    lesson 1

--- src/by/it/oliakhevich/jd01_11/Runner.java
+++ src/by/it/oliakhevich/jd01_11/Runner.java
@@ -1,17 +1,6 @@
 package by.it.oliakhevich.jd01_11;
 
 public class Runner {
-    public static void main(String[] args) {
-        ListA<String> list = new ListA<>();
-        list.add("one");
-        list.add("two");
-        list.add("three");
-        list.add("four");
-        System.out.println(list);
-        list.remove(2);
-        System.out.println(list);
-        System.out.println("get(1)="+list.get(1));
+    
 
-
-    }
 }
