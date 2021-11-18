8d1e5cba09e69c44cd2e866bb3a9787954f269a8
maksim <maksim.marukovich.00@mail.ru>
2021-11-19 00:00:45 +0300

    lesson 15 and 16 task A

---
+++ src/by/it/marukovich/jd02_01/helper/Timeout.java
@@ -0,0 +1,15 @@
+package by.it.marukovich.jd02_01.helper;
+
+import by.it.marukovich.jd02_01.Constants;
+import by.it.marukovich.jd02_01.service.StoreException;
+
+public class Timeout {
+
+    public static void sleep(int timeout) {
+        try {
+            Thread.sleep(timeout / Constants.K_SPEED);
+        } catch (InterruptedException e) {
+            throw new StoreException("Interrupted", e);
+        }
+    }
+}
