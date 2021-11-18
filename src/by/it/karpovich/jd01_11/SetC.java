ee716c8c01f066bc1c2648ec8786a2717cd14a3f
Karpovich <kdv503@gmail.com>
2021-11-18 21:37:55 +0300

    14.11.2021 ____________________________________________________________ jd01_01 - OK                                       _________ jd01_02 - ОК                                       _________ jd01_03 - OK                                       _________ jd01_04 - OK                                       _________ jd01_05 - OK                                       _________ jd01_06 - OK                                       _________ jd01_07 - OK                                       _________ jd01_08 - [Доделать С lvl]                         _________ jd01_09 - OK                                       _________ jd01_10 - OK                                       _________ jd01_11 - ОК                                       _________ jd01_12 - [С1,С2,B3]                               _________ jd01_13 - OK                                       _________ jd01_14 - [START]                                  _________ jd01_15 - [START]                                  _________ jd02_01 - [START]                                  _________ ____________________________________________________________ Test - Введение в ООП на примере создания Person и Student _ my_calculator - Создание калькулятора начало.              _ ____________________________________________________________

---
+++ src/by/it/karpovich/jd01_11/SetC.java
@@ -0,0 +1,130 @@
+package by.it.karpovich.jd01_11;
+import java.util.*;
+
+public class SetC<T> implements Set<T> {
+    private int capacity = 10;
+    private int size = 0;
+    private T[] elements = (T[]) new Object[capacity];
+
+    @Override
+    public boolean add(T t) {
+        if (!this.contains(t)) {
+            if (size == capacity - 1) {
+                capacity = (capacity * 3) / 2 + 1;
+                elements = Arrays.copyOf(elements, capacity);
+            }
+            elements[size] = t;
+            size++;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        T[] collectionArray = (T[]) c.toArray();
+        for (T t : collectionArray) {
+            this.add(t);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        if (this.contains(o)) {
+            for (int i = 0; i < this.size; i++) {
+                if (Objects.nonNull(elements[i])) {
+                    if (elements[i].equals(o)) {
+                        System.arraycopy(elements, i + 1, elements, i, size - i - 1);
+                        size--;
+                        return true;
+                    }
+                } else if (o == null) {
+                    System.arraycopy(elements, i + 1, elements, i, size - i - 1);
+                    size--;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.nonNull(elements[i])) {
+                if (elements[i].equals(o)) {
+                    return true;
+                }
+            } else if (o == null) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        T[] collectionArray = (T[]) c.toArray();
+        for (T t : collectionArray) {
+            if (!this.contains(t)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        T[] collectionArray = (T[]) c.toArray();
+        for (T t : collectionArray) {
+            this.remove(t);
+        }
+        return true;
+    }
+
+    @Override
+    public void clear() {
+        Arrays.fill(elements, null);
+        size = 0;
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(", ", "[", "]");
+        for (int i = 0; i < size; i++) {
+            out.add(String.valueOf(elements[i]));
+        }
+        return out.toString();
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+}
