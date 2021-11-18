59795ee9adcee0defb0d356c3c735fdf71e7e815
AsyaMal <asyamalyschik@gmail.com>
2021-11-18 22:03:18 +0300

    add HW Store Task A

---
+++ src/by/it/malyshchik/jd02_01/Runner.java
@@ -0,0 +1,13 @@
+package by.it.malyshchik.jd02_01;
+
+
+
+public class Runner {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            Store application = new Store();
+            application.start();
+        }
+
+    }
+}
