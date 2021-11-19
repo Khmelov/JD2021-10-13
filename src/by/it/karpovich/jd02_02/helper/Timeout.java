07008fbd6f9d27ec9bb47900998f6901dfa5f381
Karpovich <kdv503@gmail.com>
2021-11-19 11:13:47 +0300

    14.11.2021 ____________________________________________________________ jd01_01 - OK                                       _________ jd01_02 - ОК                                       _________ jd01_03 - OK                                       _________ jd01_04 - OK                                       _________ jd01_05 - OK                                       _________ jd01_06 - OK                                       _________ jd01_07 - OK                                       _________ jd01_08 - [Доделать С lvl]                         _________ jd01_09 - OK                                       _________ jd01_10 - OK                                       _________ jd01_11 - ОК                                       _________ jd01_12 - [С1,С2,B3]                               _________ jd01_13 - OK                                       _________ jd01_14 - [START]                                  _________ jd01_15 - [START]                                  _________ jd02_01 - [START]                                  _________ jd02_02 - [START]                                  _________ ____________________________________________________________ Test - Введение в ООП на примере создания Person и Student _ my_calculator - Создание калькулятора начало.              _ ____________________________________________________________

---
+++ src/by/it/karpovich/jd02_02/helper/Timeout.java
@@ -0,0 +1,15 @@
+package by.it.karpovich.jd02_02.helper;
+
+import by.it.karpovich.jd02_02.Constants;
+import by.it.karpovich.jd02_02.service.StoreException;
+
+public class Timeout {
+
+    public static void sleep(int timeout) {
+        try {
+            Thread.sleep(timeout / Constants.K_SPEED);
+        } catch (InterruptedException e) {
+            throw new StoreException("Interrupted", e);
+        }
+    }
+}
