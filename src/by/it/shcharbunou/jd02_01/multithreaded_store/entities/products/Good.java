fffab2d7d810f7655486a3676f3b4e6f77801fbd
ddshcharbunou <danielshcharbunou@gmail.com>
2021-11-24 12:20:55 +0300

    jd01_02 add Customer (need hashcode overriding)

--- src/by/it/shcharbunou/jd02_01/multithreaded_store/entities/products/Good.java
+++ src/by/it/shcharbunou/jd02_01/multithreaded_store/entities/products/Good.java
@@ -1,4 +1,54 @@
 package by.it.shcharbunou.jd02_01.multithreaded_store.entities.products;
 
+import java.math.BigDecimal;
+
 public class Good {
+
+    private String name;
+    private BigDecimal price;
+
+    public Good(String name, BigDecimal price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (this.hashCode() != obj.hashCode()) {
+            return false;
+        }
+        if (this.getClass() != obj.getClass()) {
+            return false;
+        }
+        return this.price.compareTo(((Good) obj).getPrice()) == 0 && this.name.equals(((Good) obj).getName());
+    }
+
+    @Override
+    public String toString() {
+        return "Good:\nName:" + this.name + "Price:" + this.price + "\n";
+    }
 }
--- src/by/it/shcharbunou/jd02_01/multithreaded_store/services/CustomerWorker.java
+++ src/by/it/shcharbunou/jd02_01/multithreaded_store/services/CustomerWorker.java
@@ -1,9 +1,27 @@
 package by.it.shcharbunou.jd02_01.multithreaded_store.services;
 
-public class CustomerWorker implements Runnable {
+import by.it.shcharbunou.jd02_01.multithreaded_store.entities.products.Good;
+import by.it.shcharbunou.jd02_01.multithreaded_store.services.behavior.CustomerAction;
+
+public class CustomerWorker implements Runnable, CustomerAction {
 
     @Override
     public void run() {
+
+    }
+
+    @Override
+    public void enteredStore() {
+
+    }
+
+    @Override
+    public Good chooseGood() {
+        return null;
+    }
+
+    @Override
+    public void goOut() {
 
     }
 }
