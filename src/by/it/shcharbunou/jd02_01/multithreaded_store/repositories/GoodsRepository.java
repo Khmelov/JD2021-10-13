6267f09f1d4531b2d887b4c9dae8b08482e2ca41
ddshcharbunou <danielshcharbunou@gmail.com>
2021-11-25 10:57:03 +0300

    jd01_02 add GoodsRepository

--- src/by/it/shcharbunou/jd02_01/multithreaded_store/repositories/GoodsRepository.java
+++ src/by/it/shcharbunou/jd02_01/multithreaded_store/repositories/GoodsRepository.java
@@ -2,11 +2,37 @@ package by.it.shcharbunou.jd02_01.multithreaded_store.repositories;
 
 import by.it.shcharbunou.jd02_01.multithreaded_store.entities.products.Good;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 public class GoodsRepository {
 
+    private static GoodsRepository instance;
     private List<Good> goods;
 
+    private GoodsRepository(){
+        goods = new ArrayList<>();
+        goods.add(new Good("Trojan", new BigDecimal(300)));
+        goods.add(new Good("Bot", new BigDecimal(250)));
+        goods.add(new Good("Exploit", new BigDecimal(600)));
+        goods.add(new Good("Crypt", new BigDecimal(1200)));
+        goods.add(new Good("DDOS", new BigDecimal(2000)));
+        goods.add(new Good("Access", new BigDecimal(1500)));
+    }
 
+    public static GoodsRepository getInstance() {
+        if (instance == null) {
+            instance = new GoodsRepository();
+        }
+        return instance;
+    }
+
+    public List<Good> getGoods() {
+        return goods;
+    }
+
+    public void setGoods(List<Good> goods) {
+        this.goods = goods;
+    }
 }
