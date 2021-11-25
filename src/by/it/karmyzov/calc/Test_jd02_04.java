0c88b8f85b04501403d0a08524c99f04a1b40aa7
skarmyzov <skarmyzov@gmail.com>
2021-11-25 11:32:51 +0300

    Lesson 6 started

---
+++ src/by/it/karmyzov/calc/Test_jd02_04.java
@@ -0,0 +1,14 @@
+//package by.it.karmyzov.calc;
+//
+//import org.junit.Before;
+//
+//public class Test_jd02_04 {
+//   @Before
+//    public void init () {
+//        VarRepository varRepository = new VarRepository ();
+//        Parser parser = new parser;
+//
+//    }
+//}
+//
+
