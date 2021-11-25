02a3a71928540b14fee6d72e887e2f9f6cf84a24
YekaterinaRudakova <radkevichch@gmail.com>
2021-11-25 20:19:14 +0300

    calc A

--- src/by/it/rudakova/calc/Patterns.java
+++ src/by/it/rudakova/calc/Patterns.java
@@ -1,7 +1,7 @@
 package by.it.rudakova.calc;
 
 public interface  Patterns {
-    static final String OPERATION="[-=+*/]";//поменять
+    static final String OPERATION="(?<=[^-+*/={,])[-+*/=]";
     static final String SCALAR="-?[0-9]+\\.?[0-9]*";
     static final String VECTOR="\\{("+SCALAR+",?)+}";
     static final String MATRIX="\\{("+VECTOR+",?)+}";
