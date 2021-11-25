0d4826812b06c9e25484546d5181124dfbc9b0a2
ddshcharbunou <danielshcharbunou@gmail.com>
2021-11-25 10:36:51 +0300

    jd01_02 add Randomizer

---
+++ src/by/it/shcharbunou/jd02_01/multithreaded_store/utils/Randomizer.java
@@ -0,0 +1,24 @@
+package by.it.shcharbunou.jd02_01.multithreaded_store.utils;
+
+import java.util.Random;
+
+public class Randomizer {
+
+    private final Random random;
+
+    public Randomizer() {
+        this.random = new Random();
+    }
+
+    public int randomize(int min, int max) {
+        return random.nextInt(max - min + 1) + min;
+    }
+
+    public int randomize() {
+        return random.nextInt();
+    }
+
+    public int randomize(int max) {
+        return random.nextInt(max);
+    }
+}
