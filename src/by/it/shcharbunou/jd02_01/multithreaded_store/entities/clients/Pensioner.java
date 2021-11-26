5e04b6e5b6768ab48553f42c4ffb28e72b4cf1a1
ddshcharbunou <danielshcharbunou@gmail.com>
2021-11-26 09:39:39 +0300

    jd02_01 add Pensioner and Student

---
+++ src/by/it/shcharbunou/jd02_01/multithreaded_store/entities/clients/Pensioner.java
@@ -0,0 +1,8 @@
+package by.it.shcharbunou.jd02_01.multithreaded_store.entities.clients;
+
+public class Pensioner extends Customer {
+
+    public Pensioner(int id) {
+        super(id);
+    }
+}
---
+++ src/by/it/shcharbunou/jd02_01/multithreaded_store/entities/clients/Student.java
@@ -0,0 +1,8 @@
+package by.it.shcharbunou.jd02_01.multithreaded_store.entities.clients;
+
+public class Student extends Customer {
+
+    public Student(int id) {
+        super(id);
+    }
+}
