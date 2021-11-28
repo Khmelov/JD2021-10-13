6c54a3683e9b1cb172b00ac8f95c41073ea0ee73
Karpovich <kdv503@gmail.com>
2021-11-28 13:53:43 +0300

    22.11.2021 ____________________________________________________________ jd01_01 - OK                                       _________ jd01_02 - ОК                                       _________ jd01_03 - OK                                       _________ jd01_04 - OK                                       _________ jd01_05 - OK                                       _________ jd01_06 - OK                                       _________ jd01_07 - OK                                       _________ jd01_08 - OK                                       _________ jd01_09 - OK                                       _________ jd01_10 - OK                                       _________ jd01_11 - ОК                                       _________ jd01_12 - [С2]                                  _________ jd01_13 - OK                                       _________ jd01_14 - ОК                                       _________ jd01_15 - [START]                                  _________ jd02_01 - [START]                                  _________ jd02_02 - [START]                                  _________ jd02_03 - [START]                                  _________ ____________________________________________________________ Test - Введение в ООП на примере создания Person и Student _ my_calculator - Создание калькулятора начало.              _ ____________________________________________________________

--- src/by/it/karpovich/jd01_12/TaskC1.java
+++ src/by/it/karpovich/jd01_12/TaskC1.java
@@ -12,6 +12,65 @@ package by.it.karpovich.jd01_12;
 //(оставляйте первое вхождение наименования, остальные – удаляются).
 // Выведите полученный ассоциативный массив в консоль методом toString()
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.Scanner;
+
 public class TaskC1 {
+
+
+    public static void main(String[] args) {
+
+        // init map
+        stringMap = new HashMap<>();
+        shrinkMap = new HashMap<>();
+
+        //init scanner
+        scan();
+
+        System.out.println(stringMap.toString());
+        shrink();
+        System.out.println(shrinkMap.toString());
+
+    }
+
+    static Map<Integer, String> stringMap;
+    static Map<Integer, String> shrinkMap;
+
+    public static void scan() {
+        Random rn = new Random();
+
+        Scanner scanner = new Scanner(System.in);
+        String scan =scanner.nextLine();
+        while (!scan.equals("end")) {
+            int temp = rn.nextInt();
+            while(stringMap.containsKey(temp)) {
+                temp = rn.nextInt(); // Поиск уникального числа
+            }
+            stringMap.put(temp,scan);
+            scan = scanner.nextLine();
+        }
+    }
+
+    public static void shrink() {
+        stringMap.forEach((k, v) -> {
+
+            if (!shrinkMap.containsValue(v)) {
+                shrinkMap.put(k,v);
+            }
+
+        });
+    }
+
+
+    //-> s
+    //map.add(s.hash,s)
+    //System.out.pr map.tosString
+
+    //foreach
+
+    //if
+
 
 }
