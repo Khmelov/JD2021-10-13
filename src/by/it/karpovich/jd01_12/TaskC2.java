3439d52b87b89654645309190010816990982a54
Karpovich <kdv503@gmail.com>
2021-11-28 14:40:36 +0300

    22.11.2021 ____________________________________________________________ jd01_01 - OK                                       _________ jd01_02 - ОК                                       _________ jd01_03 - OK                                       _________ jd01_04 - OK                                       _________ jd01_05 - OK                                       _________ jd01_06 - OK                                       _________ jd01_07 - OK                                       _________ jd01_08 - OK                                       _________ jd01_09 - OK                                       _________ jd01_10 - OK                                       _________ jd01_11 - ОК                                       _________ jd01_12 - [С2]                                  _________ jd01_13 - OK                                       _________ jd01_14 - ОК                                       _________ jd01_15 - [START]                                  _________ jd02_01 - [START]                                  _________ jd02_02 - [START]                                  _________ jd02_03 - [START]                                  _________ ____________________________________________________________ Test - Введение в ООП на примере создания Person и Student _ my_calculator - Создание калькулятора начало.              _ ____________________________________________________________

--- src/by/it/karpovich/jd01_12/TaskC2.java
+++ src/by/it/karpovich/jd01_12/TaskC2.java
@@ -6,6 +6,53 @@ package by.it.karpovich.jd01_12;
 // Важно: сравнивать нужно значения. Например, считается, что 1L==1.0F
 // В main покажите работоспособность решения.
 
+import java.util.*;
+
 public class TaskC2 {
 
+    public static void main(String[] args) {
+        Set<Set<Float> > list = new HashSet<>();
+
+        Set<Integer> integers = new HashSet<>();
+        integers.addAll(Arrays.asList(9, 9, 8, 8, 7, 7, 6, 5, 5, 4));
+
+        Set<Double> doubles = new HashSet<>();
+        doubles.addAll(Arrays.asList(9.5, 4.2, 5.1, 4.1, 8.1));
+
+        Set<Long> longs = new HashSet<>();
+        longs.addAll(Arrays.asList(9l, 255l, 29l));
+
+
+        list.add(listToFloat(integers));
+        list.add(listToFloat(doubles));
+        list.add(listToFloat(longs));
+
+        System.out.println(list.toString());
+    }
+
+    private static Set<Float> listToFloat(Set<? extends Number> l){
+        Set<Float> floats = new HashSet<>();
+        for (Number i: l) {
+            floats.add(i.floatValue());
+        }
+        return floats;
+    }
+
+
+    private static Set<Float> getUnion(Collection<Set<Float>> sets) {
+        Set<Float> result = new HashSet<>();
+        for (Set<Float> set: sets) {
+            result.addAll(set);
+        }
+        return result;
+    }
+
+
+    private static Set<Float> getCross(Set<Set<Float>> sets) {
+        HashSet<Float> result = new HashSet<>();
+        for (Set<Float> set: sets) {
+            result.retainAll(set);
+        }
+        return result;
+    }
 }
