3219999d5e35697bae63c038f79911650c5c1eb0
skarmyzov <skarmyzov@gmail.com>
2021-12-05 21:23:44 +0300

    Lesson 6 started

--- src/by/it/karmyzov/jd01_09/Var.java
+++ src/by/it/karmyzov/jd01_09/Var.java
@@ -1,7 +1,7 @@
 package by.it.karmyzov.jd01_09;
 
 
-abstract class Var implements Operation {
+abstract class  Var implements Operation {
     static Var createVar (String operand) {
         operand = operand.trim().replace("\\s+", "");
         if (operand.matches(Patterns.SCALAR))
