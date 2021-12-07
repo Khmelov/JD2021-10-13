accc3684186d8728329b79ac8ba145aa47e57c81
kirikant <kirikant@yandex.ru>
2021-12-07 22:54:02 +0300

    c

--- src/by/it/antipov/jd01_12/TaskC1.java
+++ src/by/it/antipov/jd01_12/TaskC1.java
@@ -1,18 +1,35 @@
 package by.it.antipov.jd01_12;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class TaskC1 {
     public static void main(String[] args) {
-        Map<String,Integer> cipher = new HashMap<>();
-        cipher.put("Kirill",1);
-        cipher.put("walking",2);
+        Random random = new Random();
+        int randomInt;
+        Map<Integer,String> cipherL = new HashMap<>();
+        Map<Integer,String> cipherS = new HashMap<>();
         Scanner scanner = new Scanner(System.in);
-        String text = scanner.next();
-        System.out.println(cipher);
+        recording(random, cipherL, scanner);
+        System.out.println(cipherL);
+        for (Map.Entry<Integer,String> entry:
+             cipherL.entrySet()) {if(!cipherS.containsValue(entry.getValue())){cipherS.put(entry.getKey(),entry.getValue());}
 
-//text.split()
+        }
+        System.out.println(cipherS);
+
+    }
+
+    private static void recording(Random random, Map<Integer, String> cipherL, Scanner scanner) {
+        int randomInt;
+        for (;;){
+            String word = scanner.nextLine();
+            if (Objects.equals(word, "end")){break;}else {
+                randomInt= random.nextInt();
+               while (cipherL.containsKey(randomInt)) {
+                   randomInt= random.nextInt();
+               }
+                cipherL.put(randomInt,word);
+            }
+        }
     }
 }
--- src/by/it/antipov/jd01_12/TaskC3.java
+++ src/by/it/antipov/jd01_12/TaskC3.java
@@ -1,5 +1,25 @@
 package by.it.antipov.jd01_12;
 
+import java.util.*;
+
 public class TaskC3 {
+    public static void main(String[] args) {
+        Map <Character,Character> brackets = new  HashMap<>();
+        Deque <Character> deque = new ArrayDeque<>();
+        int counter=0;
+        brackets.put('{','}');
+        brackets.put('(',')');
+        brackets.put('[',']');
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        char[] chars = text.toCharArray();
+        for (char charr:chars) {
+if(brackets.containsKey(charr)){deque.addFirst(charr);}
+            if(brackets.containsValue(charr)&&(deque.isEmpty())){counter++;break;}
+if(brackets.containsValue(charr)&&(brackets.get(deque.getFirst())==charr)) {deque.removeFirst();}
+        }
+if ((deque.isEmpty())&&(counter==0)){System.out.println(true);}
+ else System.out.println(false);
+    }
 
 }
