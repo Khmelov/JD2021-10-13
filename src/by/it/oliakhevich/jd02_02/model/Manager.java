ab440465c10b705d9159b28038e5b9b08d2c677e
Dmitry <ode2001@mail.ru>
2021-12-08 23:23:21 +0300

    lesson 02_02,02_03

---
+++ src/by/it/oliakhevich/jd02_02/model/Manager.java
@@ -0,0 +1,31 @@
+package by.it.oliakhevich.jd02_02.model;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Manager {
+    private AtomicInteger countIn= new AtomicInteger(0);
+
+    private AtomicInteger countOut= new AtomicInteger(0);
+
+    private final int PLAN;
+
+    public Manager(int PLAN) {
+        this.PLAN = PLAN;
+    }
+
+    public synchronized void addOneCustomer(){
+        countIn.getAndIncrement();
+    }
+
+    public synchronized void goOutOneCustomer(){
+        countOut.getAndIncrement();
+    }
+
+    public boolean storeIsOpened(){
+        return  PLAN!=countIn.get();
+    }
+
+    public boolean storeIsClosed(){
+        return PLAN==countOut.get();
+    }
+}
